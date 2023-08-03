1fa2ef5eced7644f55364116958356888aada4e4
bergmazz <ebergmann@uarts.edu>
2023-08-03 19:57:45 -0400

    wrote out reducer and action creators

--- react-app/src/store/habit.js
+++ react-app/src/store/habit.js
@@ -4,3 +4,73 @@ const SUGGESTED = 'routine/SUGGESTED';
 const ADD_HABIT = 'routine/ADD_HABIT';
 const UPDATE_HABIT = 'routine/UPDATE_HABIT';
 const DELETE_HABIT = 'routine/DELETE_HABIT';
+
+// Action Creators
+export const setUserHabits = ( habits ) => ( {
+    type: USER_HABITS,
+    payload: habits,
+} );
+
+export const setAllHabits = ( habits ) => ( {
+    type: ALL_HABITS,
+    payload: habits,
+} );
+
+export const setSuggestedHabits = ( habits ) => ( {
+    type: SUGGESTED,
+    payload: habits,
+} );
+
+export const addHabit = ( habit ) => ( {
+    type: ADD_HABIT,
+    payload: habit,
+} );
+
+export const updateHabit = ( habit ) => ( {
+    type: UPDATE_HABIT,
+    payload: habit,
+} );
+
+export const deleteHabitbyId = ( habitId ) => ( {
+    type: DELETE_HABIT,
+    payload: habitId,
+} );
+
+// Thunks
+
+
+// Reducer
+const initialState = {
+    userHabits: [],
+    allHabits: [],
+    suggestedHabits: [],
+};
+
+const habitReducer = ( state = initialState, action ) => {
+    switch ( action.type ) {
+        case USER_HABITS:
+            return { ...state, userHabits: [ ...action.payload ] };
+        case ALL_HABITS:
+            return { ...state, allHabits: [ ...action.payload ] };
+        case SUGGESTED:
+            return { ...state, suggestedHabits: [ ...action.payload ] };
+        case ADD_HABIT:
+            return { ...state, userHabits: [ ...state.userHabits, action.payload ] };
+        case UPDATE_HABIT:
+            return {
+                ...state,
+                userHabits: state.userHabits.map( ( habit ) =>
+                    habit.id === action.payload.id ? action.payload : habit
+                ),
+            };
+        case DELETE_HABIT:
+            return {
+                ...state,
+                userHabits: state.userHabits.filter( ( habit ) => habit.id !== action.payload ),
+            };
+        default:
+            return state;
+    }
+};
+
+export default habitReducer;
