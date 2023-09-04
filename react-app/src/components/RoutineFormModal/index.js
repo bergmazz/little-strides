236b9a0f74952af96c311f5af66d095ea06f3278
bergmazz <ebergmann@uarts.edu>
2023-09-04 14:56:03 -0400

    can access submit button now and tested that step 1 is still working from separate file

--- react-app/src/components/RoutineFormModal/index.js
+++ react-app/src/components/RoutineFormModal/index.js
@@ -383,12 +383,14 @@ function RoutineFormModal ( { routines } ) {
                             <p>Or please provide a valid image URL starting with http:// or https:// and ending with png, jpg, jpeg, gif, or svg.</p>
                         ) }
                         {/* <img className="cover" src="" onClick={ () => { setCoverImage( "" ) } } /> */ }
+                        <div className="covers-imgs">
                         <img className="cover1" src={ image1 } onClick={ () => { setCoverImage( "https://images.pexels.com/photos/345522/pexels-photo-345522.jpeg" ) } } />
                         <img className="cover2" src={ image2 } onClick={ () => { setCoverImage( "https://images.pexels.com/photos/3900437/pexels-photo-3900437.jpeg" ) } } />
-                        <img className="cover3" src={ image3 } onClick={ () => { setCoverImage( "  https://images.pexels.com/photos/2627945/pexels-photo-2627945.jpeg" ) } } />
+                            <img className="cover3" src={ image3 } onClick={ () => { setCoverImage( "https://images.pexels.com/photos/2627945/pexels-photo-2627945.jpeg" ) } } />
                         <img className="cover4" src={ image4 } onClick={ () => { setCoverImage( "https://images.pexels.com/photos/4388593/pexels-photo-4388593.jpeg" ) } } />
-                        <img className="cover5" src={ image5 } onClick={ () => { setCoverImage( "  https://images.pexels.com/photos/2649403/pexels-photo-2649403.jpeg" ) } } />
+                            <img className="cover5" src={ image5 } onClick={ () => { setCoverImage( "https://images.pexels.com/photos/2649403/pexels-photo-2649403.jpeg" ) } } />
                         <img className="cover6" src={ image6 } onClick={ () => { setCoverImage( "https://images.pexels.com/photos/2309266/pexels-photo-2309266.jpeg" ) } } />
+                        </div>
                         <input
                             type="text"
                             value={ coverImage }
